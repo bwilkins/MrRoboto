99c425f9d4b3aa7adc3265b01f3342e9051a237b
Brett Wilkins <brett@brett.geek.nz>
2014-05-04 13:17:11 +1000

    [Tests] Fixing tests which would only run with the last test given

--- spec/integration/all_files_spec.rb
+++ spec/integration/all_files_spec.rb
@@ -9,23 +9,27 @@ describe MrRoboto::Runner do
   let(:args) { [input_file] }
   let(:input_file) { 'foobar' }
 
+
   context 'when given a non-existent file' do
     it 'will return a file-missing error' do
       expect(described_class.new.run(args)).to eq "No such file or directory #{input_file}"
     end
   end
 
+  shared_examples_for 'a robot program' do |test_input_file, test_output_file|
+    let(:input_file)  { File.join(File.dirname(__FILE__), test_input_file) }
+    let(:output_file) { File.join(File.dirname(__FILE__), test_output_file) }
+
+    let(:expected_output) { File.read(output_file).chomp }
+
+    it "reads that file (#{test_input_file}) and returns the expected output" do
+      expect(described_class.new.run(args)).to eq expected_output
+    end
+  end
+
   context 'when given a file with instructions' do
     ['robot_test_01', 'robot_test_02', 'robot_test_03'].each do |testname|
-      let(:input_file)  { File.join(File.dirname(__FILE__),"#{testname}.input") }
-      let(:output_file) { File.join(File.dirname(__FILE__),"#{testname}.output") }
-
-      let(:expected_output) { File.read(output_file).chomp }
-
-      it "reads that file (#{testname}.input) and returns the expected output" do
-        expect(described_class.new.run(args)).to eq expected_output
-      end
-
+      it_behaves_like 'a robot program', "#{testname}.input", "#{testname}.output"
     end
   end
 
